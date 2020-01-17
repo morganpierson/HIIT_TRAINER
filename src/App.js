1305c22a08b51fa993ec5376273ebe3b1d1c7f34
Morgan Pierson <morganpierson@gmail.com>
2020-01-17 11:16:59 -0800

    initial commit, react boilerplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>H I I T &nbsp;&nbsp;|&nbsp;&nbsp; T R A I N E R</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
